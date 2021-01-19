e4b8921825bc0de3eee8570c059532b2f5eaeef5
nel417 <n1ck@hey.com>
2021-01-18 19:00:46 -0600

    initial commit, crud functionality, data models

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css"
+import axios from 'axios';
 
 export default class CreateExercise extends Component {
   constructor(props) {
@@ -16,10 +17,15 @@ export default class CreateExercise extends Component {
   }
   
   componentDidMount() {
-    this.setState({
-      users:['Nick'],
-      username: 'Test User'
-    })
+   axios.get('http://localhost:5000/users')
+   .then(res => {
+     if(res.data.length > 0) {
+       this.setState({
+         users: res.data.map(user => user.username),
+         username: res.data[0].username
+       })
+     }
+   })
   }
 
   onChangeUsername = (e) => {
@@ -56,6 +62,11 @@ export default class CreateExercise extends Component {
     }
 
     console.log(exercise);
+
+    axios.post('http://localhost:5000/exercises/add', exercise)
+    .then(res => console.log(res.data))
+
+
     window.location = '/';
   } 
 
